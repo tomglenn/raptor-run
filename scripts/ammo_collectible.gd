eb4a27989ccc7e27c7364f32a76f4a6e6bcd28c1
Tom Glenn <tom.e.glenn@gmail.com>
2022-08-24 22:26:02 +0100

    Adding the ammo collectible

---
+++ scripts/ammo_collectible.gd
@@ -0,0 +1,24 @@
+extends Area2D
+
+@export var value = 3
+@onready var player = $"/root/World/Player"
+@onready var sprite = $AnimatedSprite2D
+@onready var pickup_sound = $PickupSound
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	self.body_entered.connect(_on_body_entered)
+
+func _on_body_entered(body):
+	if body.is_in_group("player"):
+		player.add_ammo(value)
+		pickup_sound.play()
+		sprite.play("collected")
+		sprite.animation_finished.connect(_on_animation_finished)
+		
+func _on_animation_finished():
+	queue_free()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
